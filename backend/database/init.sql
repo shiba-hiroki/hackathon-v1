c6bbf422c9dd58f79fcdff452fe81d59566481b2
nakstsu <koki20001206@outllok.jp>
2024-02-20 10:01:02 +0000

    add database

---
+++ backend/database/init.sql
@@ -0,0 +1,7 @@
+-- サンプルデータの挿入
+INSERT INTO users (id, name, type, hashedpassword)
+VALUES
+    ('01EFG1T51E6YH9VMPZGZ6WHTJW', 'John Doe', 'employee', 'hashedpassword123'),
+    ('01EFG1T51E6YH9VMPZGZ6WHTJK', 'Jane Smith', 'employer', 'hashedpassword456'),
+    ('01EFG1T51E6YH9VMPZGZ6WHTJZ', 'Bob Johnson', 'employee', 'hashedpassword789');
+
