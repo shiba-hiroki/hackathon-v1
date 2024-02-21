767379245e788646a923873774de30e645ef4506
nakstsu <koki20001206@outllok.jp>
2024-02-21 10:22:55 +0000

    add entity

--- src/entity/AttendanceRecord.ts
+++ src/entity/AttendanceRecord.ts
@@ -1,18 +1,18 @@
 import z from "zod";
 
 const AttendanceRecordParser = z.object({
-  userID: z.string(),
-  time: z.date(),
-  state: z.enum(["checkIn", "checkOut", "brakeStart", "brakeEnd"]),
+	userID: z.string(),
+	time: z.date(),
+	state: z.enum(["checkIn", "checkOut", "brakeStart", "brakeEnd"]),
 });
 
 export type AttendanceRecord = z.infer<typeof AttendanceRecordParser>;
 
 export const parseAsAttendanceRecord = (
-  data: any,
+	data: unknown,
 ): AttendanceRecord | Error => {
-  const validationResponse = AttendanceRecordParser.safeParse(data);
-  return validationResponse.success
-    ? validationResponse.data
-    : validationResponse.error;
+	const validationResponse = AttendanceRecordParser.safeParse(data);
+	return validationResponse.success
+		? validationResponse.data
+		: validationResponse.error;
 };
