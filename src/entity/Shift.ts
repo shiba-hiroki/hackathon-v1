6ab793b6a54fc52632a7090bedb42f70635c6d13
nakstsu <koki20001206@outllok.jp>
2024-02-23 01:13:15 +0000

    fix zod

--- src/entity/Shift.ts
+++ src/entity/Shift.ts
@@ -1,26 +1,26 @@
 import z from "zod";
 
-const ShiftParser = z.object({
+const Shift = z.object({
 	userID: z.string(),
 	startTime: z.date(),
 	endTime: z.date(),
 });
 
-export type ShiftRequest = z.infer<typeof ShiftParser>;
+export type ShiftRequest = z.infer<typeof Shift>;
 
 export const parseAsShiftRequest = (data: unknown): ShiftRequest | Error => {
-	const validationResponse = ShiftParser.safeParse(data);
+	const validationResponse = Shift.safeParse(data);
 	return validationResponse.success
 		? validationResponse.data
 		: validationResponse.error;
 };
 
-export type ConfirmedShift = z.infer<typeof ShiftParser>;
+export type ConfirmedShift = z.infer<typeof Shift>;
 
 export const parseAsConfirmedShift = (
 	data: unknown,
 ): ConfirmedShift | Error => {
-	const validationResponse = ShiftParser.safeParse(data);
+	const validationResponse = Shift.safeParse(data);
 	return validationResponse.success
 		? validationResponse.data
 		: validationResponse.error;
