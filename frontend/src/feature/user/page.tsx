6544478f5cdcf042166e5dc35ca2d41728697cf8
nakstsu <koki20001206@outllok.jp>
2024-03-08 11:08:55 +0000

    add user 画面

--- frontend/src/feature/user/page.tsx
+++ frontend/src/feature/user/page.tsx
@@ -46,10 +46,12 @@ export function User() {
   }, []);
 
   const handleUserDelete = async (id: number) => {
-    // await client.DELETE("/api/employer/user/{id}", {
-    //   params: { path: { id: id.toString() } },
-    // });
-    console.log(`Button clicked for row with id ${id}`);
+    const { response } = await client.DELETE("/api/employer/user/{id}", {
+      params: { path: { id: id.toString() } },
+    });
+    if (response.status === StatusCodes.OK) {
+      setUsers(users?.filter((u) => u.id !== id));
+    }
   };
 
   const handleCreateUser = async () => {
@@ -68,98 +70,103 @@ export function User() {
 
   return (
     <div className="w-5/6 h-screen p-10">
-      <h2 className="font-medium size-16 w-full text-center">USER LIST</h2>
-      <div className="flex items-center justify-center">
-        <IconContext.Provider value={{ color: "#22d3ee", size: "30px" }}>
-          <button
-            type="button"
-            onClick={() => setShowModal(true)}
-            className="hover:opacity-30 mb-2"
-          >
-            <PiUserPlus />
-          </button>
-        </IconContext.Provider>
-      </div>
-      {showModal && (
-        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
-          <div className="z-10 bg-white p-4 rounded-lg shadow-md">
-            <h2 className="text-2xl font-bold mb-4">Create User</h2>
-            <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700">
-                Name
-              </label>
-              <input
-                type="text"
-                name="name"
-                value={newUser.name}
-                onChange={(e) => {
-                  setNewUser({
-                    ...newUser,
-                    [e.target.name]: e.target.value,
-                  });
-                }}
-                className="mt-1 p-2 w-full border rounded-md"
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700">
-                Password
-              </label>
-              <input
-                type="password"
-                name="password"
-                value={newUser.password}
-                onChange={(e) => {
-                  setNewUser({
-                    ...newUser,
-                    [e.target.name]: e.target.value,
-                  });
-                }}
-                className="mt-1 p-2 w-full border rounded-md"
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700">
-                User Type
-              </label>
-              <select
-                name="type"
-                value={newUser.type}
-                onChange={(e) => {
-                  setNewUser({
-                    ...newUser,
-                    [e.target.name]: e.target.value,
-                  });
-                }}
-                className="mt-1 p-2 w-full border rounded-md"
-              >
-                <option value="employee">Employee</option>
-                <option value="employer">Employer</option>
-              </select>
-            </div>
-            <div className="flex justify-end">
-              <button
-                type="button"
-                onClick={() => setShowModal(false)}
-                className="mr-2 p-2 bg-gray-300 rounded-md hover:opacity-30"
-              >
-                Cancel
-              </button>
-              <button
-                type="button"
-                onClick={handleCreateUser}
-                className="p-2 bg-cyan-400 text-white rounded-md hover:opacity-30"
-              >
-                Create
-              </button>
+      <div className="h-1/6 flex gap-y-2 justify-center items-center flex-col">
+        <h2 className="font-medium w-full text-center">USER LIST</h2>
+        <div className="flex items-center justify-center">
+          <IconContext.Provider value={{ color: "#22d3ee", size: "30px" }}>
+            <button
+              type="button"
+              onClick={() => setShowModal(true)}
+              className="hover:opacity-30"
+            >
+              <PiUserPlus />
+            </button>
+          </IconContext.Provider>
+        </div>
+        {showModal && (
+          <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
+            <div className="z-10 bg-white p-4 rounded-lg shadow-md">
+              <h2 className="text-2xl font-bold mb-4">Create User</h2>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  value={newUser.name}
+                  onChange={(e) => {
+                    setNewUser({
+                      ...newUser,
+                      [e.target.name]: e.target.value,
+                    });
+                  }}
+                  className="mt-1 p-2 w-full border rounded-md"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  value={newUser.password}
+                  onChange={(e) => {
+                    setNewUser({
+                      ...newUser,
+                      [e.target.name]: e.target.value,
+                    });
+                  }}
+                  className="mt-1 p-2 w-full border rounded-md"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  User Type
+                </label>
+                <select
+                  name="type"
+                  value={newUser.type}
+                  onChange={(e) => {
+                    setNewUser({
+                      ...newUser,
+                      [e.target.name]: e.target.value,
+                    });
+                  }}
+                  className="mt-1 p-2 w-full border rounded-md"
+                >
+                  <option value="employee">Employee</option>
+                  <option value="employer">Employer</option>
+                </select>
+              </div>
+              <div className="flex justify-end">
+                <button
+                  type="button"
+                  onClick={() => setShowModal(false)}
+                  className="mr-2 p-2 bg-gray-300 rounded-md hover:opacity-30"
+                >
+                  Cancel
+                </button>
+                <button
+                  type="button"
+                  onClick={handleCreateUser}
+                  className="p-2 bg-cyan-400 text-white rounded-md hover:opacity-30"
+                >
+                  Create
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
       {users ? (
-        <div className="w-full flex flex-wrap justify-between">
+        <div className="h-5/6 overflow-y-auto flex flex-wrap content-start justify-start">
           {users.map((user) => (
-            <div key={user.id} className="rounded shadow-md p-3 w-1/3 flex-col">
+            <div
+              key={user.id}
+              className="rounded h-1/4 shadow-md p-3 w-1/3 flex-col"
+            >
               <div className="flex items-center justify-center">
                 <IconContext.Provider value={{ size: "30px" }}>
                   {user.type === "employer" ? (
